3fbdaa1810fe3368f676cf80ee878ac1164dcd52
InigoPena <i.pena@opendeusto.es>
2024-05-18 11:35:56 +0200

    LogFile implemented

---
+++ FicheroLog/logFlie.h
@@ -0,0 +1,22 @@
+#ifndef LOGFILE_H
+#define LOGFILE_H
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include <ctime>
+
+enum LogLevel { INFO, WARNING, ERROR };
+
+class Log{
+    public:
+        Log(const std::string& filename);
+        ~Log();
+        void writeLog(LogLevel level, const std::string& message);
+    private:
+        std::ofstream logFile;
+        std::string getTimestamp();
+        std::string levelToString(LogLevel level);
+};
+
+#endif
